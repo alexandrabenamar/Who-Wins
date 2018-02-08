99f1471ef80b547ee4a3533184e19759ddb98c74
miraaitsaada <mira.ait.94@gmail.com>
2018-02-08 13:12:50 +0100

    ajout lexicon

---
+++ lexicon/lexicon_weights.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 23:05:14 2018
+
+@author: mira
+"""
+
+
+
+import pandas as pd
+
+
+#fonction qui retourne la somme des poids selon le dico pondéré
+def weight_positive_negative (sentence):
+    df = pd.read_table("documents/senticnet4.txt", header=None);
+    
+    lexicon = df.iloc[: , [0,2]]
+    
+    pos_neg=0
+    
+    for i in sentence:
+        print(i)
+        weight=lexicon.loc[(lexicon[0] == i)][2].tolist()
+        if len(weight)>0:
+            pos_neg=pos_neg + weight[0]
+    return pos_neg
+        
+
+#test
+sentence = ["apples","are","good","for","health","but","bad","in","flavor"]
+print(weight_positive_negative(sentence))
+
