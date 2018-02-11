40e39ffa27365ebcd8f4491cd45c368ade89ee12
alexandrabenamar <alexandra.benamar@yahoo.Fr>
2018-02-11 20:27:41 +0100

    100% termine - a voir pour garder les stopwords

--- Twitter_data/nettoyage.py
+++ Twitter_data/nettoyage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import codecs
+
 def cleaning(sentence):
     """ Data cleansing :
         # Removal of usernames (marked by @ symbol)
@@ -20,14 +22,7 @@ def cleaning(sentence):
     trash_words = []
     numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being",
-    "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have",
-    "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've",
-    "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought",
-    "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their",
-    "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under",
-    "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's",
-    "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+    stopwords = codecs.open('/Users/alexandrabenamar/Who-Wins/dataset/stopwords.txt', mode='r').read().split('\n')
 
     flag=0
     for i in range(0, spaces+1):
@@ -55,10 +50,54 @@ def cleaning(sentence):
         sentence=sentence.replace(word+" ","")
 
     sentence=sentence.strip()                              # removal of spaces before and after the sentence
+    if not check_language(sentence):
+        return None
+
     return sentence
 
 
+def check_language(sentence):
+    """
+        Cheking if the language of the tweets is english
+        Dictionnary : http://www.mit.edu/~ecprice/wordlist.10000
+        Output :
+            # true if the sentence is in english
+            # false otherwise
+    """
+    eng_words = codecs.open('/Users/alexandrabenamar/Who-Wins/dataset/english_dictionnary.txt', mode='r').read().split('\n')
+
+    words = toList(sentence)
+
+    english_words = 0
+    for word in words:
+        if word in eng_words:
+            english_words+=1
+
+    return (english_words > 2)
+
+def toList(sentence):
+    """
+        Transform a sentence into a list of words
+            # Input : sentence to convert
+            # Output : list of words
+    """
+    words = []
+    tmp = []
+    for char in sentence:
+        if char == " ":
+            words.append(tmp)
+            tmp=""
+        else:
+            tmp += char
+    if tmp:
+        words.append(tmp)
+
+    return words
+
 if __name__ == "__main__":
-    sentence="@ Mira RT: 1 the brexit will never ever pass or even be talked about, sorry guys, not gonna happen ! 3462892 http://www.google.fr # Brexit # Sortie2016"
+    sentence="@ Mira RT: 1 the brexit will never ever pass or even be talked about, sorry guys, not gonna happen ! http://www.google.fr # Brexit # Sortie2016"
+    sentence=cleaning(sentence)
+    print sentence
+    sentence = "je mange des frites"
     sentence=cleaning(sentence)
     print sentence
