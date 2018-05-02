136477048ca2f5a70e92faa242fa09fc2b63ebd9
miraaitsaada <mira.ait.94@gmail.com>
2018-05-03 00:49:05 +0200

    brexit_test

--- Learning_methods/NeuralNetwork.py
+++ Learning_methods/NeuralNetwork.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 25 00:01:22 2018
+Created on Tue Apr 24 23:41:07 2018
 
 @author: mira
 """
 
-from pyspark.ml.classification import LinearSVC
+from pyspark.ml.classification import MultilayerPerceptronClassifier
 from functions_ml import spark_context, training_set, test_set, write_result, brexit_labeled_data, model_predict
 
 
-def SVC_train(training):
+def MLP_train(training):
     
-    trainer_SVC = LinearSVC(maxIter=10, regParam=0.1)
-    model = trainer_SVC.fit(rescaledData)
+    num_cols = training.select('features').collect()[0].features.size  #vocabulary size
+    layers = [num_cols , 100 , 2]
+    MLP_trainer = MultilayerPerceptronClassifier(maxIter=100, layers=layers, blockSize=128, seed=1234)
+    model = MLP_trainer.fit(training)
     
     return model
 
@@ -26,8 +28,8 @@ if __name__ == "__main__":
     print("Training...\n")
     
     (rescaledData, idfModel) = training_set(sc = sc, numFeatures = numFeatures)
-    model = SVC_train(training = rescaledData)
-
+    model = MLP_train(training = rescaledData)
+    
     print("Test... \n")
 
 #    rescaled_test_df = test_set(sc, numFeatures = numFeatures, idfModel = idfModel)
@@ -39,4 +41,6 @@ if __name__ == "__main__":
    
     print("Saving results...\n")
     
-    write_result(1, 1, accuracy = accuracy, f1 = f1, name = "LinearSVC")
+    write_result(1, 1, accuracy = accuracy, f1 = f1, name = "Neural Network")
+    
+    
